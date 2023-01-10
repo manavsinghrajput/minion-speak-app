0135175a32d361fc045031c86df03cc1503c0b15
Manvendra Singh <47238236+manavsinghrajput@users.noreply.github.com>
2023-01-10 12:34:30 +0530

    input,server processing and output

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtArea = document.querySelector("#txt-area");
 var txtOutput = document.querySelector("#output");
 
-// txtOutput.innerText = "manav"; - gives the output
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getUrl(input) {
+    return url + "?" + "text=" + input;
+}
 
 function clickHandler() {
-    txtOutput.innerText = "minion lang. " + txtArea.value; //takes input from the textarea and gives output on the screen
+    // txtOutput.innerText = "minion lang. " + txtArea.value; //takes input from the textarea and gives output on the screen
+    var txtInput = txtArea.value;
+    fetch(getUrl(txtInput))
+        .then(response => response.json())
+        .then(json => {
+            var translatedTxt = json.contents.translated;
+            txtOutput.innerText = translatedTxt;
+        })
 };
 btnTranslate.addEventListener("click", clickHandler)
