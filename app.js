8109d8042820fd8a02fd7f6d63b72577bfaba38f
Manvendra Singh <47238236+manavsinghrajput@users.noreply.github.com>
2023-01-10 08:46:02 +0530

    input processing and output

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtArea = document.querySelector("#txt-area");
+var txtOutput = document.querySelector("#output");
+
+// txtOutput.innerText = "manav"; - gives the output
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input", txtArea.value);
+    txtOutput.innerText = "minion lang. " + txtArea.value; //takes input from the textarea and gives output on the screen
 };
 btnTranslate.addEventListener("click", clickHandler)
